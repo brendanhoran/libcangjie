73136c4bd02756ef09a69ebb1ffdefc7a54708b2
Mathieu Bridon <bochecha@fedoraproject.org>
2014-04-15 12:49:28 +0800

    cangjie-char: Rewrite with GObject

---
+++ src/cangjie-char.c
@@ -0,0 +1,193 @@
+/* Copyright (c) 2013-2014 - The libcangjie authors.
+ *
+ * This file is part of libcangjie.
+ *
+ * libcangjie is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libcangjie is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libcangjie.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <cangjie-char.h>
+
+
+struct _CangjieCharPrivate
+{
+    gchar *chchar;
+    gchar *simpchar;
+    gchar *code;
+    guint  frequency;
+};
+
+G_DEFINE_TYPE_WITH_PRIVATE (CangjieChar, cangjie_char, G_TYPE_OBJECT)
+
+enum
+{
+    PROP_0,
+
+    PROP_CANGJIE_CHAR_CHCHAR,
+    PROP_CANGJIE_CHAR_SIMPCHAR,
+    PROP_CANGJIE_CHAR_CODE,
+    PROP_CANGJIE_CHAR_FREQUENCY,
+
+    N_PROPERTIES
+};
+
+/* Keep a pointer to the properties definition */
+static GParamSpec *obj_properties[N_PROPERTIES] = { NULL, };
+
+static void
+cangjie_char_set_property (GObject      *object,
+                           guint         property_id,
+                           const GValue *value,
+                           GParamSpec   *pspec)
+{
+    CangjieChar *self = CANGJIE_CHAR (object);
+
+    switch (property_id)
+    {
+        case PROP_CANGJIE_CHAR_CHCHAR:
+            g_free (self->priv->chchar);
+            // TODO: Ensure only 4 bytes
+            self->priv->chchar = g_value_dup_string (value);
+            break;
+
+        case PROP_CANGJIE_CHAR_SIMPCHAR:
+            g_free (self->priv->simpchar);
+            // TODO: Ensure only 4 bytes
+            self->priv->simpchar = g_value_dup_string (value);
+            break;
+
+        case PROP_CANGJIE_CHAR_CODE:
+            g_free (self->priv->code);
+            // TODO: Ensure only 6 bytes
+            self->priv->code = g_value_dup_string (value);
+            break;
+
+        case PROP_CANGJIE_CHAR_FREQUENCY:
+            self->priv->frequency = g_value_get_uint (value);
+            break;
+
+        default:
+            /* We don't have any other property... */
+            G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+            break;
+    }
+}
+
+static void
+cangjie_char_get_property (GObject    *object,
+                           guint       property_id,
+                           GValue     *value,
+                           GParamSpec *pspec)
+{
+    CangjieChar *self = CANGJIE_CHAR (object);
+
+    switch (property_id)
+    {
+        case PROP_CANGJIE_CHAR_CHCHAR:
+            g_value_set_string (value, self->priv->chchar);
+            break;
+
+        case PROP_CANGJIE_CHAR_SIMPCHAR:
+            g_value_set_string (value, self->priv->simpchar);
+            break;
+
+        case PROP_CANGJIE_CHAR_CODE:
+            g_value_set_string (value, self->priv->code);
+            break;
+
+        case PROP_CANGJIE_CHAR_FREQUENCY:
+            g_value_set_uint (value, self->priv->frequency);
+            break;
+
+        default:
+            /* We don't have any other property... */
+            G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+           break;
+    }
+}
+
+static void
+cangjie_char_dispose (GObject *object)
+{
+    /* Always chain up to the parent class; there is no need to check if
+     * the parent class implements the dispose() virtual function: it is
+     * always guaranteed to do so
+     */
+    G_OBJECT_CLASS (cangjie_char_parent_class)->dispose (object);
+}
+
+static void
+cangjie_char_finalize (GObject *object)
+{
+    CangjieChar *self = CANGJIE_CHAR (object);
+
+    g_free (self->priv->chchar);
+    g_free (self->priv->simpchar);
+    g_free (self->priv->code);
+
+    /* Always chain up to the parent class; as with dispose(), finalize()
+     * is guaranteed to exist on the parent's class virtual function table
+     */
+    G_OBJECT_CLASS (cangjie_char_parent_class)->finalize (object);
+}
+
+static void
+cangjie_char_class_init (CangjieCharClass *klass)
+{
+    GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+
+    gobject_class->set_property = cangjie_char_set_property;
+    gobject_class->get_property = cangjie_char_get_property;
+
+    gobject_class->dispose = cangjie_char_dispose;
+    gobject_class->finalize = cangjie_char_finalize;
+
+    obj_properties[PROP_CANGJIE_CHAR_CHCHAR] =
+        g_param_spec_string ("chchar",
+                             "Character",
+                             "The character to input",
+                             "",
+                             G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE);
+
+    obj_properties[PROP_CANGJIE_CHAR_SIMPCHAR] =
+        g_param_spec_string ("simpchar",
+                             "Simplified Character",
+                             "The Simplified Chinese equivalent of the character to input",
+                             "",
+                             G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE);
+
+    obj_properties[PROP_CANGJIE_CHAR_CODE] =
+        g_param_spec_string ("code",
+                             "Code",
+                             "The Cangjie code of the character to input",
+                             "",
+                             G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE);
+
+    obj_properties[PROP_CANGJIE_CHAR_FREQUENCY] =
+        g_param_spec_uint ("frequency",
+                           "Frequency",
+                           "The character frequency",
+                           0, 65535, 0,
+                           G_PARAM_CONSTRUCT_ONLY | G_PARAM_READWRITE);
+
+    g_object_class_install_properties (gobject_class,
+                                       N_PROPERTIES,
+                                       obj_properties);
+}
+
+static void
+cangjie_char_init (CangjieChar *self)
+{
+    self->priv = cangjie_char_get_instance_private (self);
+}
---
+++ src/cangjie-char.h
@@ -0,0 +1,67 @@
+/* Copyright (c) 2013-2014 - The libcangjie authors.
+ *
+ * This file is part of libcangjie.
+ *
+ * libcangjie is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libcangjie is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libcangjie.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/* inclusion guard */
+#ifndef __CANGJIE_CHAR_H__
+#define __CANGJIE_CHAR_H__
+
+#include <glib-object.h>
+/*
+ * Potentially, include other headers on which this header depends.
+ */
+
+/*
+ * Type macros.
+ */
+#define CANGJIE_TYPE_CHAR                  (cangjie_char_get_type ())
+#define CANGJIE_CHAR(obj)                  (G_TYPE_CHECK_INSTANCE_CAST ((obj), CANGJIE_TYPE_CHAR, CangjieChar))
+#define CANGJIE_IS_CHAR(obj)               (G_TYPE_CHECK_INSTANCE_TYPE ((obj), CANGJIE_TYPE_CHAR))
+#define CANGJIE_CHAR_CLASS(klass)          (G_TYPE_CHECK_CLASS_CAST ((klass), CANGJIE_TYPE_CHAR, CangjieCharClass))
+#define CANGJIE_IS_CHAR_CLASS(klass)       (G_TYPE_CHECK_CLASS_TYPE ((klass), CANGJIE_TYPE_CHAR))
+#define CANGJIE_CHAR_GET_CLASS(obj)        (G_TYPE_INSTANCE_GET_CLASS ((obj), CANGJIE_TYPE_CHAR, CangjieCharClass))
+
+typedef struct _CangjieChar        CangjieChar;
+typedef struct _CangjieCharClass   CangjieCharClass;
+typedef struct _CangjieCharPrivate CangjieCharPrivate;
+
+struct _CangjieChar
+{
+    /* Parent instance structure */
+    GObject parent_instance;
+
+    /* instance members */
+    CangjieCharPrivate *priv;
+};
+
+struct _CangjieCharClass
+{
+    /* Parent class structure */
+    GObjectClass parent_class;
+
+    /* class members */
+};
+
+/* used by CANGJIE_TYPE_CHAR */
+GType cangjie_char_get_type (void);
+
+/*
+ * Method definitions.
+ */
+
+#endif /* __CANGJIE_CHAR_H__ */
