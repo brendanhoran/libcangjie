0edd4d1677ca84f1bd973acc7d494f77bbc77bca
Mathieu Bridon <bochecha@fedoraproject.org>
2014-04-15 23:03:18 +0800

    Add a basic cangie.h header
    Nothing here yet, it's coming...

---
+++ src/cangjie.h
@@ -0,0 +1,26 @@
+/* Copyright (c) 2013-2014 - The libcangjie authors.
+ *
+ * This file is part of libcangjie.
+ *
+ * libcangjie is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libcangjie is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libcangjie.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/* inclusion guard */
+#ifndef __CANGJIE_H__
+#define __CANGJIE_H__
+
+#include <cangjie-char.h>
+
+#endif
