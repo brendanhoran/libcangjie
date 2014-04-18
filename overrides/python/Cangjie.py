67ca9f2d0068a41f49445da7a18d2a2e30459255
Mathieu Bridon <bochecha@fedoraproject.org>
2014-04-18 13:42:19 +0800

    Add a PyGI override for the CangjieContext
    Just like the CangjieChar, this merely makes the API nicer to use, at
    least when it comes to accessing/setting the properties.

--- overrides/python/Cangjie.py
+++ overrides/python/Cangjie.py
@@ -24,3 +24,21 @@ class Char(Cangjie.Char):
 
 Char = override(Char)
 __all__.append("Char")
+
+
+class Context(Cangjie.Context):
+    def __getattr__(self, attr):
+        if attr in ("filter_flags", "version"):
+            return self.get_property(attr)
+
+        raise AttributeError("'%s' object has no attribute '%s'"
+                             % (type(self).__name__, attr))
+
+    def __setattr__(self, attr, value):
+        if attr in ("filter_flags", "version"):
+            return self.set_property(attr, value)
+
+        Cangjie.Context.__setattr__(self, attr, value)
+
+Context = override(Context)
+__all__.append("Context")
